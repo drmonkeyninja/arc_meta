000125f1dd4fda63cacee0bbb2e13a609210a415
Andy Carter <andy1704@gmail.com>
2015-11-18 19:40:59 +0000

    This fixes #2

--- src/arc_meta.php
+++ src/arc_meta.php
@@ -438,9 +438,11 @@ if (@txpinterface == 'admin') {
     register_callback('_arc_meta_category_meta', 'category_ui', 'extend_detail_form');
     register_callback('_arc_meta_category_meta_save', 'category', 'cat_article_save');
 
-    add_privs('arc_meta_section_tab', '1,2,3,4');
-    register_tab($prefs['arc_meta_section_tab'], 'arc_meta_section_tab', 'Sections Meta Data');
-    register_callback('arc_meta_section_tab', 'arc_meta_section_tab');
+    if (!empty($prefs['arc_meta_section_tab'])) {
+        add_privs('arc_meta_section_tab', '1,2,3,4');
+        register_tab($prefs['arc_meta_section_tab'], 'arc_meta_section_tab', 'Sections Meta Data');
+        register_callback('arc_meta_section_tab', 'arc_meta_section_tab');
+    }
 }
 
 function _arc_meta_install()
